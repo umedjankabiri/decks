543b1a122f17cc9dbbfe60cbfb20507536bd4e9e
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 01:46:11 +0300

    Add DeckResponseProps type definition
    Introduced DeckResponseProps type to encapsulate deck items and pagination properties. This new type consolidates DeckProps and PaginationProps, providing a clearer structure for deck-related responses.

---
+++ src/features/decks/types/DeckResponseProps.ts
@@ -0,0 +1,26 @@
+export type DeckResponseProps = {
+  items: DeckProps[],
+  pagination: PaginationProps
+}
+type AuthorProps = {
+  id: string;
+  name: string;
+}
+type PaginationProps = {
+  currentPage: number;
+  itemsPerPage: number;
+  totalPages: number;
+  totalItems: number;
+}
+export type DeckProps = {
+  id: string;
+  userId: string;
+  isFavorite: boolean;
+  author: AuthorProps,
+  name: string;
+  isPrivate: boolean;
+  cover: string;
+  created: string;
+  updated: string;
+  cardsCount: number;
+}
