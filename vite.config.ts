9814f394932a4e0d446c075730c431cd5e30aa03
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 01:45:35 +0300

    Add path aliases for app and features
    This update includes path aliases in the Vite configuration to simplify the import statements for the 'app' and 'features' directories. This improves code readability and maintainability by allowing shortened and consistent import paths.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      app: path.resolve(__dirname, "src/app"),
+      features: path.resolve(__dirname, "src/features/"),
+    }
+  }
 })
