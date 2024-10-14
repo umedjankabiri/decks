e4d160c66e10dfc78b021075c08b841e5a3c66d0
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 01:46:28 +0300

    Enable fetching and displaying decks in DecksList component
    Integrated Redux state with DecksList component to fetch and display decks. Added hooks for dispatching actions and retrieving decks from the store. Implemented useEffect to trigger API calls and update the state accordingly.

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,25 @@
 import s from './DecksList.module.css'
+import { useEffect } from 'react'
+import { decksApi } from 'features/decks/decks-api.ts'
+import { useAppDispatch, useAppSelector } from 'app/store.ts'
+import { selectDecks } from 'features/decks/decks-selectors.ts'
+import { DeckItem } from 'features/decks/DecksList/DeckItem/DeckItem.tsx'
+import { setDecksAC } from 'features/decks/decks-reducer.ts'
 
 export const DecksList = () => {
-  return <ul className={s.list}></ul>
+  const { decks } = useAppSelector(selectDecks)
+  const dispatch = useAppDispatch()
+
+  if (!decks) return null
+  useEffect(() => {
+    decksApi.getDecks().then((response) => dispatch(setDecksAC(response.data.items)))
+  }, [])
+
+  return (
+    <ul className={s.list}>
+      {decks.map((deck) => (
+        <DeckItem key={deck.id} deck={deck} />
+      ))}
+    </ul>
+  )
 }
