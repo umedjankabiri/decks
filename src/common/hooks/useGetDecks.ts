898db3d11500635c0c6c248d14e6bc80e3294232
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 22:10:20 +0300

    Add useGetDecks hook
    Introduce useGetDecks hook to encapsulate deck fetching logic. This hook utilizes useAppDispatch and useAppSelector to trigger deck fetching and select deck data from the store, ensuring a cleaner and reusable code structure.

---
+++ src/common/hooks/useGetDecks.ts
@@ -0,0 +1,15 @@
+import { useAppDispatch, useAppSelector } from 'app/store.ts'
+import { selectDecks } from 'features/decks/decks-selectors.ts'
+import { useEffect } from 'react'
+import { getDecksTC } from 'features/decks/decks-thunks.ts'
+
+export const useGetDecks = () => {
+  const { decks } = useAppSelector(selectDecks)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getDecksTC())
+  }, [dispatch])
+
+  return { decks }
+}
