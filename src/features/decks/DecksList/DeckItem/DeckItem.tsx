44d7872dfb684101c3a27c273ab38c022b9dfc05
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 00:47:00 +0300

    Add delete and update actions to DeckItem component
    Implement functionality for deleting and updating decks with loading states. Introduce dispatch hooks and event handlers to manage deck operations.

--- src/features/decks/DecksList/DeckItem/DeckItem.tsx
+++ src/features/decks/DecksList/DeckItem/DeckItem.tsx
@@ -1,10 +1,29 @@
 import s from './DeckItem.module.css'
 import { DeckProps } from 'common/types/DeckResponseProps.ts'
+import { useAppDispatch } from 'app/store.ts'
+import { deleteDeckTC, updateDeckTC } from 'features/decks/deckThunks.ts'
+import { useState } from 'react'
 
 const TEST_ACC_NAME = 'Nik-Kik-Shpink'
 
 export const DeckItem = ({ deck }: { deck: DeckProps }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
   const isTestingDeck = deck.author.name === TEST_ACC_NAME
+  const dispatch = useAppDispatch()
+
+  const onClickDeleteHandler = () => {
+    setIsLoading(true)
+    dispatch(deleteDeckTC(deck.id)).finally(()=> {
+      setIsLoading(false)
+    })
+  }
+  const onClickUpdateHandler = () => {
+    setIsLoading(true)
+    dispatch(updateDeckTC({id: deck.id, name: `Updated name`})).finally(()=> {
+      setIsLoading(false)
+    })
+  }
 
   return (
     <li className={s.item}>
@@ -24,8 +43,8 @@ export const DeckItem = ({ deck }: { deck: DeckProps }) => {
 
       {isTestingDeck && (
         <div className={s.buttonBox}>
-          <button>update</button>
-          <button>delete</button>
+          <button disabled={isLoading} onClick={onClickUpdateHandler}>update</button>
+          <button disabled={isLoading} onClick={onClickDeleteHandler}>delete</button>
         </div>
       )}
     </li>
