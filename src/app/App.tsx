2d147a528d9b0fb95287d06d49d1f15cdbce93b8
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 23:01:40 +0300

    Add loading indicator to App component
    Integrated a LinearLoader component to display a loading indicator when the status is "loading". Enhanced the App component by incorporating state management using useAppSelector and Redux selectors.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
 import { Decks } from '../features/decks/Decks.tsx'
+import { LinearLoader } from 'common/components/Loader/LinearLoader.tsx'
+import { useEffect } from 'react'
+import { decksApi } from 'features/decks/decksApi.ts'
+import { useAppDispatch, useAppSelector } from 'app/store.ts'
+import { selectStatus } from 'features/decks/decksSelectors.ts'
 
 export const App = () => {
+  const { status } = useAppSelector(selectStatus)
+
   return (
     <div>
+      {status == "loading" && <LinearLoader />}
       <Decks />
     </div>
   )
