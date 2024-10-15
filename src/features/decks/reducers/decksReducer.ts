c4c1e210b09904137c98164f5bd717b18feffbad
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 00:46:12 +0300

    Add delete and update deck actions in decksReducer
    Implemented cases for `DELETE_DECK` and `UPDATE_DECK` actions in the `decksReducer` to handle deck removal and updates. Also, added new action creators `deleteDeckAC` and `updateDeckAC` for dispatching these actions.

--- src/features/decks/reducers/decksReducer.ts
+++ src/features/decks/reducers/decksReducer.ts
@@ -19,14 +19,25 @@ export const decksReducer = (state: DeckStateProps = initialState, action: Decks
     case 'ADD_DECKS':
       return {
         ...state,
-        decks: [action.payload.decks, ...state.decks]
+        decks: [action.payload.decks, ...state.decks],
+      }
+    case 'DELETE_DECK':
+      return {
+        ...state,
+        decks: state.decks.filter((deck) => deck.id !== action.payload.id),
+      }
+    case 'UPDATE_DECK':
+      return {
+        ...state,
+        decks: state.decks.map((deck) =>
+          (deck.id === action.payload.deck.id ? {...deck, name: action.payload.deck.name} : deck)),
       }
     default:
       return state
   }
 }
 
-export const setDecksAC = (decks: DeckProps[]) =>
-  ({ type: 'SET_DECKS', payload: { decks: decks } }) as const
-export const addDecksAC = (decks: DeckProps) =>
-  ({ type: 'ADD_DECKS', payload: { decks: decks} }) as const
+export const setDecksAC = (decks: DeckProps[]) => ({ type: 'SET_DECKS', payload: { decks: decks } }) as const
+export const addDecksAC = (decks: DeckProps) => ({ type: 'ADD_DECKS', payload: { decks: decks } }) as const
+export const deleteDeckAC = (id: string) => ({ type: 'DELETE_DECK', payload: { id: id } }) as const
+export const updateDeckAC = (deck: DeckProps) => ({ type: 'UPDATE_DECK', payload: { deck: deck } }) as const
