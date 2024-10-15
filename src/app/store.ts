92b4718ece2b13cdffcf395c55cdb395b8a76f1e
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 22:55:33 +0300

    Add appReducer to the rootReducer
    Include appReducer to manage application state within the Redux store. Additionally, extend AppActions type to incorporate StatusActions to handle status-related actions.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,14 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
 import { thunk, ThunkDispatch } from 'redux-thunk'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { decksReducer } from 'features/decks/reducers/decks-reducer.ts'
+import { decksReducer } from 'features/decks/reducers/decksReducer.ts'
 import { DecksActions } from 'common/types/DecksActionProps.ts'
+import { appReducer } from 'features/decks/reducers/appReducer.ts'
+import { StatusActions } from 'common/types/StatusActionProps.ts'
 
 const rootReducer = combineReducers({
   decksReducer,
+  appReducer
 })
 
 // @ts-ignore
@@ -13,7 +16,7 @@ export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 
 export type AppRootState = ReturnType<typeof rootReducer>
 export type AppDispatch = ThunkDispatch<AppRootState, unknown, AppActions>
-export type AppActions = DecksActions
+export type AppActions = DecksActions | StatusActions
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<AppRootState> = useSelector
