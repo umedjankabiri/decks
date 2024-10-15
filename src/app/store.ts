1dc61be0908bc089eb22a1611e385637aaeca20b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 22:09:31 +0300

    Add middleware support to Redux store
    Integrated Redux Thunk middleware to handle async actions. Corrected import paths for reducers and action types for better consistency.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,15 @@
-import { combineReducers, legacy_createStore } from 'redux'
-import { ThunkDispatch } from 'redux-thunk'
+import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
+import { thunk, ThunkDispatch } from 'redux-thunk'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { decksReducer } from '../features/decks/decks-reducer.ts'
-import { DecksActions } from 'features/decks/types/DecksActionProps.ts'
+import { decksReducer } from 'features/decks/reducers/decks-reducer.ts'
+import { DecksActions } from 'common/types/DecksActionProps.ts'
 
 const rootReducer = combineReducers({
   decksReducer,
 })
 
-export const store = legacy_createStore(rootReducer)
+// @ts-ignore
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 
 export type AppRootState = ReturnType<typeof rootReducer>
 export type AppDispatch = ThunkDispatch<AppRootState, unknown, AppActions>
