028e92cdd1a941b124cfe529954f2647cc2b80a7
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 00:46:03 +0300

    Add CRUD actions for decks
    Implemented `deleteDeckTC` and `updateDeckTC` thunks to support deleting and updating deck items. Enhanced error handling and status management for every deck-related action. This helps in making the deck feature more comprehensive and robust.

--- src/features/decks/deckThunks.ts
+++ src/features/decks/deckThunks.ts
@@ -1,30 +1,48 @@
 import { AppDispatch } from 'app/store.ts'
 import { decksApi } from 'features/decks/decksApi.ts'
-import { addDecksAC, setDecksAC } from 'features/decks/reducers/decksReducer.ts'
-import { AddDeckParamsProps } from 'common/types/DeckResponseProps.ts'
+import { addDecksAC, deleteDeckAC, setDecksAC, updateDeckAC } from 'features/decks/reducers/decksReducer.ts'
+import { AddDeckParamsProps, UpdateDeckParamsProps } from 'common/types/DeckResponseProps.ts'
 import { setErrorAC, setStatusAC } from 'features/decks/reducers/statusReducer.ts'
 
-export const getDecksTC = () =>
-  async (dispatch: AppDispatch) => {
-    dispatch(setStatusAC("loading"))
-    try {
+export const getDecksTC = () => async (dispatch: AppDispatch) => {
+  dispatch(setStatusAC('loading'))
+  try {
     const response = await decksApi.getDecks()
-      dispatch(setDecksAC(response.data.items))
-      dispatch(setStatusAC("succeeded"))
+    dispatch(setDecksAC(response.data.items))
+    dispatch(setStatusAC('succeeded'))
   } catch (error) {
-    dispatch(setStatusAC("failed"))
-    dispatch(setErrorAC("some error occurred"))
+    dispatch(setStatusAC('failed'))
+    dispatch(setErrorAC('some error occurred'))
   }
 }
-export const AddDeckTC = (params: AddDeckParamsProps) =>
-  async (dispatch: AppDispatch) => {
-    dispatch(setStatusAC("loading"))
-    try {
+export const AddDeckTC = (params: AddDeckParamsProps) => async (dispatch: AppDispatch) => {
+  dispatch(setStatusAC('loading'))
+  try {
     const response = await decksApi.addDeck(params)
-      dispatch(addDecksAC(response.data))
-      dispatch(setStatusAC("succeeded"))
+    dispatch(addDecksAC(response.data))
+    dispatch(setStatusAC('succeeded'))
   } catch (error) {
-    dispatch(setStatusAC("failed"))
-    dispatch(setErrorAC("some error occurred"))
+    dispatch(setStatusAC('failed'))
+    dispatch(setErrorAC('some error occurred'))
+  }
+}
+export const deleteDeckTC = (id: string) => async (dispatch: AppDispatch) => {
+  dispatch(setStatusAC('loading'))
+  try {
+    const response = await decksApi.deleteDeck(id)
+    dispatch(deleteDeckAC(response.data.id))
+    dispatch(setStatusAC('succeeded'))
+  } catch (error) {
+    dispatch(setStatusAC('failed'))
+  }
+}
+export const updateDeckTC = (params: UpdateDeckParamsProps) => async (dispatch: AppDispatch) => {
+  dispatch(setStatusAC('loading'))
+  try {
+    const response = await decksApi.updateDeck(params)
+    dispatch(updateDeckAC(response.data))
+    dispatch(setStatusAC('succeeded'))
+  } catch (error) {
+    dispatch(setStatusAC('failed'))
   }
 }
