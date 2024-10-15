60304f556164343c2bb9308f1406a41c4071aaed
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 22:55:00 +0300

    Refactor thunks with async/await and error handling
    Converted deck thunks to use async/await for better readability and added error handling. Status and error actions are now dispatched to provide feedback on operation statuses.

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -1,16 +1,34 @@
 import { AppDispatch } from 'app/store.ts'
-import { decksApi } from 'features/decks/decks-api.ts'
-import { addDecksAC, setDecksAC } from 'features/decks/reducers/decks-reducer.ts'
+import { decksApi } from 'features/decks/decksApi.ts'
+import { addDecksAC, setDecksAC } from 'features/decks/reducers/decksReducer.ts'
 import { AddDeckParamsProps } from 'common/types/DeckResponseProps.ts'
+import { setErrorAC, setStatusAC } from 'features/decks/reducers/appReducer.ts'
 
-export const getDecksTC = () => (dispatch: AppDispatch) => {
-  decksApi.getDecks().then(response => {
-    dispatch(setDecksAC(response.data.items))
-  })
+export const getDecksTC = () =>
+  async (dispatch: AppDispatch) => {
+  try {
+    const response = await decksApi.getDecks()
+    if (response.data.items.length) {
+      dispatch(setStatusAC("loading"))
+      dispatch(setDecksAC(response.data.items))
+      dispatch(setStatusAC("succeeded"))
+    }
+  } catch (error) {
+    dispatch(setStatusAC("failed"))
+    dispatch(setErrorAC("some error occurred"))
+  }
 }
 export const AddDeckTC = (params: AddDeckParamsProps) =>
   async (dispatch: AppDispatch) => {
-  decksApi.addDeck(params).then(response => {
-    dispatch(addDecksAC(response.data))
-  })
+  try {
+    const response = await decksApi.addDeck(params)
+    if (response.data) {
+      dispatch(setStatusAC("loading"))
+      dispatch(addDecksAC(response.data))
+      dispatch(setStatusAC("succeeded"))
+    }
+  } catch (error) {
+    dispatch(setStatusAC("failed"))
+    dispatch(setErrorAC("some error occurred"))
+  }
 }
