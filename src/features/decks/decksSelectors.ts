6ae87d278a5e04f86a8082c3e5313eb69cc207de
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 23:01:21 +0300

    Add selectStatus selector
    Introduce a new selector, `selectStatus`, for accessing the status state. This enhances readability and modularity by encapsulating the status retrieval logic in a single function.

--- src/features/decks/decksSelectors.ts
+++ src/features/decks/decksSelectors.ts
@@ -1,4 +1,6 @@
 import { AppRootState } from 'app/store.ts'
 import { DeckStateProps } from 'common/types/DeckStateProps.ts'
+import { StatusStateProps } from 'common/types/StatusStateProps.ts'
 
 export const selectDecks = (state: AppRootState): DeckStateProps => state.decksReducer
+export const selectStatus = (state: AppRootState): StatusStateProps => state.statusReducer
