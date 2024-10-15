8f00cbccce56ad312c83841fcde96409f365f877
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 22:10:08 +0300

    Refactor DecksList to use custom hook for fetching decks
    Replaced manual API call and state management in DecksList with the custom useGetDecks hook. This simplifies the component by reducing the lines of code and making it easier to maintain.

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,22 +1,9 @@
 import s from './DecksList.module.css'
-import { useEffect } from 'react'
-import { decksApi } from 'features/decks/decks-api.ts'
-import { useAppDispatch, useAppSelector } from 'app/store.ts'
-import { selectDecks } from 'features/decks/decks-selectors.ts'
 import { DeckItem } from 'features/decks/DecksList/DeckItem/DeckItem.tsx'
-import { setDecksAC } from 'features/decks/decks-reducer.ts'
+import { useGetDecks } from 'common/hooks/useGetDecks.ts'
 
 export const DecksList = () => {
-  const { decks } = useAppSelector(selectDecks)
-  const dispatch = useAppDispatch()
-
-  if (!decks) return null
-  useEffect(() => {
-    decksApi.getDecks().then((response) => {
-      if (!response.data.items) return;
-      dispatch(setDecksAC(response.data.items))
-    })
-  }, [dispatch])
+  const {decks} = useGetDecks()
 
   return (
     <ul className={s.list}>
