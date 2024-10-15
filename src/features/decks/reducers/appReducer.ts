49b64a4d2a8924fa479ccf6ae7b9f4f8937382d8
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-15 22:54:52 +0300

    Add error handling to app reducer
    Updated the app reducer to handle error status along with the existing request status. Introduced a new action `SET_ERROR` and modified the `SET_STATUS` action to include payload objects.

--- src/features/decks/reducers/appReducer.ts
+++ src/features/decks/reducers/appReducer.ts
@@ -1,20 +1,29 @@
 import { RequestStatusType, StatusStateProps } from 'common/types/StatusStateProps.ts'
+import { StatusActions } from 'common/types/StatusActionProps.ts'
 
 const initialState: StatusStateProps = {
   status: 'idle' as RequestStatusType,
   error: null
 }
 
-export const appReducer = (state: StatusStateProps = initialState, action: ActionsType): StatusStateProps => {
+export const appReducer = (state: StatusStateProps = initialState, action: StatusActions): StatusStateProps => {
   switch (action.type) {
+    case 'SET_STATUS':
+      return {
+        ...state,
+        status: action.payload.status
+      }
+      case 'SET_ERROR':
+        return {
+          ...state,
+          error: action.payload.error
+        }
     default:
       return state
   }
 }
 
-export const setStatusAC = (status: RequestStatusType) =>
-  ({type: "SET_STATUS", payload: status} as const)
+export const setStatusAC = (status: RequestStatusType) => 
+  ({ type: 'SET_STATUS', payload: { status: status } }) as const
 export const setErrorAC = (error: string | null) =>
-  ({type: "SET_ERROR", payload: error} as const)
-
-type ActionsType = any
+  ({ type: 'SET_ERROR', payload: { error: error } }) as const
