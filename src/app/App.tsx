8bfd80b5fd8164a5a61b4259b6df7f45c68a0d93
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 14:45:35 +0300

    Simplify App.tsx imports and fix loader condition
    Removed unnecessary imports from App.tsx to clean up the code. Also fixed the loading condition to use single quotes for consistency.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
 import './App.css'
 import { Decks } from '../features/decks/Decks.tsx'
 import { LinearLoader } from 'common/components/Loader/LinearLoader.tsx'
-import { useEffect } from 'react'
-import { decksApi } from 'features/decks/decksApi.ts'
-import { useAppDispatch, useAppSelector } from 'app/store.ts'
+import { useAppSelector } from 'app/store.ts'
 import { selectStatus } from 'features/decks/decksSelectors.ts'
 import { GlobalError } from 'common/components/GlogalError/GlobalError.tsx'
 
@@ -12,7 +10,7 @@ export const App = () => {
 
   return (
     <div>
-      {status == "loading" && <LinearLoader />}
+      {status == 'loading' && <LinearLoader />}
       <Decks />
       <GlobalError />
     </div>
