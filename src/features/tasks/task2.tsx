79ec0e93d93ade827bf9d07a7f35bde3a276cd48
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 14:44:04 +0300

    Add MyComponent with generic prop support
    Introduced MyComponent that accepts generic props to enhance reusability across different data types. Updated App component to include examples of MyComponent usage with string and user objects.

---
+++ src/features/tasks/task2.tsx
@@ -0,0 +1,30 @@
+type MyComponentProps<T> = {
+  items: T[]
+  defaultItem: T
+}
+function MyComponent<T, >(props: MyComponentProps<T>) {
+  console.log(props)
+  return <p>some content</p>
+}
+
+const App = () => {
+  const users: User[] = [
+    { name: 'Bilbo', age: 111 },
+    { name: 'Frodo', age: 33 },
+  ]
+
+  return (
+    <>
+      <MyComponent items={['react', 'typescript']} defaultItem={'javascript'} />
+      <MyComponent items={users} defaultItem={{
+        name: "Sam",
+        age: 42
+      }} />
+    </>
+  )
+}
+
+type User = {
+  name: string
+  age: number
+}
