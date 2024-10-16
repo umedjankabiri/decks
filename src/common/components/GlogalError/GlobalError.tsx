665e5efb5591deff8bd932797d2b410ebce6703c
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 14:43:35 +0300

    Integrate error handling with Redux in GlobalError
    Refactor GlobalError component to use Redux for error management. Added error selector and action dispatcher to handle error messages more effectively and reset state.

--- src/common/components/GlogalError/GlobalError.tsx
+++ src/common/components/GlogalError/GlobalError.tsx
@@ -1,12 +1,17 @@
 import { useEffect } from 'react'
 import { toast, ToastContainer } from 'react-toastify'
+import { useAppDispatch, useAppSelector } from 'app/store.ts'
+import { selectError } from 'features/decks/decksSelectors.ts'
+import { setErrorAC } from 'features/decks/reducers/statusReducer.ts'
 
 export const GlobalError = () => {
-  const errorMessage = ''
+  const errorMessage = useAppSelector(selectError)
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
     if (errorMessage) {
       toast.error(errorMessage)
+      dispatch(setErrorAC(null))
     }
   }, [errorMessage])
 
