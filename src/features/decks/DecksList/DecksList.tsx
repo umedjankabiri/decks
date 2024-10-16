b6a272a3aee3c78f429d8671730e62c2b9e9034b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-16 14:43:45 +0300

    Add loading skeleton to DecksList component
    Introduce DeckItemSkeleton to display placeholders while decks are loading. This enhances the user experience by showing a visual indicator during data fetching.

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,15 +1,17 @@
 import s from './DecksList.module.css'
 import { DeckItem } from 'features/decks/DecksList/DeckItem/DeckItem.tsx'
 import { useGetDecks } from 'common/hooks/useGetDecks.ts'
+import { DeckItemSkeleton } from 'features/decks/DecksList/DeckItem/DeckItemSkeleton.tsx'
 
 export const DecksList = () => {
-  const {decks} = useGetDecks()
+  const { decks, isLoading } = useGetDecks()
 
   return (
-    <ul className={s.list}>
-      {decks.map((deck) => (
-        <DeckItem key={deck.id} deck={deck} />
-      ))}
-    </ul>
+      <ul className={s.list}>
+        {!isLoading && decks.length === 0 && <DeckItemSkeleton count={decks.length} />}
+        {decks.map((deck) => (
+          <DeckItem key={deck.id} deck={deck} />
+        ))}
+      </ul>
   )
 }
